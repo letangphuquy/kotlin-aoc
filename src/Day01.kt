dbb38d672d50e7645a00026d7918534a6899dcdf
Le Tang Phu Quy <letangphuquy4@gmail.com>
2024-03-15 08:57:52 +0700

    Migrate Day01 2022

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getSumList(input: List<String>): List<Int> {
+        var curSum = 0
+        val listSums = mutableListOf(0)
+        for (line in input) {
+            if ("" == line) {
+                listSums.add(curSum)
+                curSum = 0
+            } else curSum += line.toInt()
+        }
+        listSums.sortWith(compareByDescending { it })
+        return listSums
+//        curSum = 0
+//        for (i in 0..2) curSum += listSums[i]
+//
+//        return input.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<Int>): Int = input[0]
+    fun part2(input: List<Int>): Int = input.subList(0,3).sumOf { it }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // test if implementation meet criteria from the description, like:
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) > 0)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val list = getSumList(input)
+    part1(list).println()
+    part2(list).println()
 }
